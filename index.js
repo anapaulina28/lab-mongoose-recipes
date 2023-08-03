91c28231e82e26a195befbfd9d1c48a2741c2d2f
John Doe <johndoe@example.com>
2023-08-03 17:32:08 +0100

    lab done

--- index.js
+++ index.js
@@ -1,12 +1,11 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
 // Import of the model Recipe from './models/Recipe.model.js'
-const Recipe = require('./models/Recipe.model');
+const Recipe = require("./models/Recipe.model");
 // Import of the data from './data.json'
-const data = require('./data');
+const data = require("./data");
 
-const MONGODB_URI = 'mongodb://localhost:27017/recipe-app';
-
+const MONGODB_URI = "mongodb://127.0.0.1/recipe-app";
 //Method 1 : Using Async Await
 
 const manageRecipes = async () => {
@@ -17,6 +16,39 @@ const manageRecipes = async () => {
 
     // Before adding any recipes to the database, let's remove all existing ones
     await Recipe.deleteMany();
+
+    const cookie = {
+      title: "Chocolate chip cookie",
+      level: "Easy Peasy",
+      ingredients: [
+        "Butter",
+        "Sugar",
+        "Vanilla Extract",
+        "Flour",
+        "Eggs",
+        "Chocolate Chips",
+      ],
+      cuisine: "American",
+      dishType: "dessert",
+      image:
+        "https://www.ihearteating.com/wp-content/uploads/2016/08/Easiest-Chocolate-Chip-Cookies-800-2.jpg",
+      duration: 18,
+      creator: "Kate Bush",
+    };
+    let newRecipe = await Recipe.create(cookie);
+    // console.log(newRecipe.title)
+
+    let insertedRecipes = await Recipe.insertMany(data);
+
+    let newRigatoni = await Recipe.findOneAndUpdate(
+      { duration: 220 },
+      { duration: 100 }
+    );
+
+    let deleteCake = await Recipe.deleteOne({ title: "Carrot Cake" });
+
+    let databaseClose = await mongoose.connection.close(MONGODB_URI);
+    console.log("Closed Database");
 
     // Run your code here, after you have insured that the connection was made
   } catch (error) {
